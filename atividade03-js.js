91ce6e01b4ec2cb2bd5268a09991c8aae637ba10
HenthonyGuilherme <henthonyguilhermeferreira.2021@gmail.com>
2021-10-01 17:54:51 -0300

    O-0

--- atividade03-js.js
+++ atividade03-js.js
@@ -13,12 +13,14 @@ function contar(string){
     return tras;
   }
   
+  // 3º)
+
   function positivo(n1,n2,n3,n4,n5){
-    if (n1 > 0 && n2 > 0 && n3 > 0 && n4 > 0 && n5 > 0){
-      var soma = n1 + n2 + n3 + n4 + n5;
-      return 'A soma dos numeros é: ' + soma;
-    }
-    else{
-      return 'ERRO, função só recebe números inteiros positivos';
-    }
+  if (n1 > 0 && n2 > 0 && n3 > 0 && n4 > 0 && n5 > 0 && n1 %1 == 0 && n2 %1 == 0 && n3 %1 == 0 && n4 %1 == 0 && n5 %1 == 0){
+    var soma = n1 + n2 + n3 + n4 + n5;
+    return 'A soma dos numeros é: ' + soma;
   }
+  else{
+    return 'ERRO, função só recebe números inteiros positivos';
+  }
+}
